30b05e57e08dc409c57830ef3ef26ee59d4e894d
Liam Brenner <franzymope@gmail.com>
2017-02-25 12:18:15 -0500

    Finished basic exercises for formatting book titles and converting seconds to a timer format.

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,27 @@
 class Book
-# write your code here
+
+def title
+	@title
 end
+
+def title=(title)
+	@title = format(title)
+end
+
+private
+
+def format(title)
+	lower_words = %w(and if but or on in the of a an)
+	title_words = title.split(" ")
+	title_words.each do |word|
+		if lower_words.include? (word) and word != title_words[0]
+			next
+		else 
+			word.capitalize!
+		end
+	end
+	@title = title_words.join(" ")
+end
+
+end
+
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,48 @@
 class Timer
-  #write your code here
+  
+def initialize 
+	@seconds = 0.0
 end
+
+def seconds
+	@seconds
+end
+
+def seconds=(value)
+	@seconds = value
+end
+
+def time_string
+	counter(@seconds)
+end
+
+private
+
+def counter(seconds)
+	counter_array = []
+	
+	if seconds == 0
+		counter_array << "00" << "00" << "00"
+	elsif seconds > 0
+		hour_tens = seconds/36000
+		seconds = seconds%36000
+		hour_ones = seconds/3600
+		seconds = seconds%3600
+		min_tens = seconds/600
+		seconds = seconds%600
+		min_ones = seconds/60
+		seconds = seconds%60
+		sec_tens = seconds/10
+		seconds = seconds%10
+		sec_ones = seconds
+		counter_array << hour_tens.to_s + hour_ones.to_s << min_tens.to_s + min_ones.to_s << sec_tens.to_s + sec_ones.to_s
+	end	
+
+	return counter_array.join(":")
+end
+
+end
+
+s = Timer.new
+s.seconds = 45467
+puts s.time_string
